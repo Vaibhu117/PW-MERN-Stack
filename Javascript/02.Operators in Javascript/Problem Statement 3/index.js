b11a8d2159e21c3e94ebb8546271133cbcf96c7d
vaibhav chougule <122853291+Vaibhu117@users.noreply.github.com>
2024-01-18 14:42:32 +0530

    Operators in Javascript assignment solved.

---
+++ Javascript/02.Operators in Javascript/Problem Statement 3/index.js
@@ -0,0 +1,9 @@
+const number = 10;
+number > 0
+? console.log("Positive")
+:number < 0
+? console.log("Negative")
+: console.log("Zero");
+
+// output 
+// Positive
---
+++ Javascript/02.Operators in Javascript/Problem Statement 4/index.js
@@ -0,0 +1,5 @@
+const age = 18;
+age < 18 ? console.log("You cannot vote") : console.log("You can vote");
+
+// output 
+// You can vote
